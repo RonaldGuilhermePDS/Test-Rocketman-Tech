34e427e29737da11aad9548703976c312b1de438
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-06-10 20:46:54 -0300

    feat: add export pokemons controller and service

--- server/src/main.py
+++ server/src/main.py
@@ -14,9 +14,37 @@ async def get_pokemons (
 ):
   return await pokemon_service.list(offset, limit)
 
-@app.get("/pokemons/{pokemon_name}")
+@app.get("/pokemons/details/{pokemon_name}")
 async def get_pokemon_details(
   pokemon_name: str,
   pokemon_service: PokemonService = Depends()
 ):
   return await pokemon_service.get_details(pokemon_name)
+
+@app.get("/pokemons/export")
+async def export_pokemons(
+   pokemon_service: PokemonService = Depends()
+):
+    from xml.etree import ElementTree as ET
+
+    root = ET.Element("Pokemons")
+
+    pokemons = await pokemon_service.list(0, 20)
+
+    for pokemon in pokemons:
+        details = await pokemon_service.get_details(pokemon['name'])
+
+        pokemon_elem = ET.SubElement(root, "Pokemon")
+
+        ET.SubElement(pokemon_elem, "Name").text = details["name"]
+        ET.SubElement(pokemon_elem, "Height").text = str(details["height"])
+        ET.SubElement(pokemon_elem, "Weight").text = str(details["weight"])
+        ET.SubElement(pokemon_elem, "Types").text = ", ".join(details["types"])
+        ET.SubElement(pokemon_elem, "Abilities").text = ", ".join(details["abilities"])
+
+    tree = ET.ElementTree(root)
+
+    with open("pokemons.xml", "wb") as f:
+        tree.write(f)
+
+    return {"message": "Export Successful"}
